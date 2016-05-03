bef7a91ef86cb73694c987f657819a12a770d593
ALan <alanwmail@163.com>
2016-05-03 23:55:00 +0800

    upadte

--- jimi/settings.py
+++ jimi/settings.py
@@ -78,7 +78,6 @@ WSGI_APPLICATION = 'jimi.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jimi',
         'HOST': 'localhost',
@@ -126,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'statics'),
+)
