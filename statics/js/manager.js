463703848484df0ef16d96393736e8484f65c922
ALan <alanwmail@163.com>
2016-05-22 23:20:54 +0800

    checkbox 选项框更新

--- statics/js/manager.js
+++ statics/js/manager.js
@@ -1,21 +1,45 @@
 /**
  * Created by root on 2016/5/15.
  */
-function DataDel(argv){
-
+//删除
+function GetCheck(){
+    var id_list = [];
+    $.each($(":checkbox"),function(){
+        if ($(this).prop('checked')){
+            console.log($(this));
+            var current = $(this).siblings().first();
+            //id_list.append(current)
+            console.log(current)
+        }
+    });
+    //console.log(id_list)
 }
 
+//function DataDel(argv){
+//    if (GetCheck()){
+//
+//    }
+//
+//}
+
+//全选
 function CheckAll(){
-    $.each($("input"),function(k,v){
-        $(this).attr('checked','checked')
+    $.each($("input"),function(){
+        $(this).prop('checked',true)
     })
 }
 
-
+//取消全选
 function CancleALL(){
-    $.each($("input"),function(k,v){
-        if($(this).attr('checked')){
-            $(this).removeAttr('checked')
+    $.each($("input"),function(){
+        if($(this).prop('checked')){
+            $(this).prop('checked',false)
         }
     })
 }
+
+
+//++++++++++++++++++++++++摸态框++++++++++++++++++++++
+//$("#myModal").modal().css({
+//                 "margin-top":$(this).height()/3}
+//);
